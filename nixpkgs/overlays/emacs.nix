f32ef1acf385b6b54616a53cbd98ed3e79993064
Michael Lingelbach <m.j.lbach@gmail.com>
2020-06-07 23:46:33 -0700

    Version emacs overlay with niv

--- nixpkgs/overlays/emacs.nix
+++ nixpkgs/overlays/emacs.nix
@@ -1,11 +1,9 @@
 let
+  sources = import ../nix/sources.nix;
   fetchGitHubArchive = { owner, repo, rev, sha256 }: builtins.fetchTarball {
     url = "https://github.com/${owner}/${repo}/archive/${rev}.tar.gz";
     inherit sha256;
   };
 in import (fetchGitHubArchive {
-  owner = "nix-community";
-  repo = "emacs-overlay";
-  rev = "a7456b41281507d4f6838cedf89290f4f3b4e02f";
-  sha256 = "17a1cd0xfhhhmnjglasixb7dmdgyibxwllddw7r1id1vvbsz1zxg";
+  inherit (sources.emacs-overlay) owner repo rev sha256;
 })

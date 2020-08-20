680c5db974af8e50269a2075591d3ac7e4889c13
Michael Lingelbach <m.j.lbach@gmail.com>
2020-08-20 12:10:04 -0700

    Add flake

--- nixpkgs/machines/fedora/config.nix
+++ nixpkgs/machines/fedora/config.nix
@@ -1,4 +1,5 @@
 {
   allowUnfree = true;
   cudaSupport = true;
+  # experimental-features = nix-command flakes;
 }
--- nixpkgs/overlays/emacs.nix
+++ nixpkgs/overlays/emacs.nix
@@ -13,6 +13,13 @@ let
   emacs-overlay = (import (fetchGitHubArchive {
     inherit (sources.emacs-overlay) owner repo rev sha256;
   }) self super);
+  # emacs-overlay = (import (fetchGitHubArchive {
+  #   owner = "nix-community";
+  #   repo = "emacs-overlay";
+  #   rev = "413dc6ac8945769e996df118778be64a5c6da48e";
+  #   sha256= "09pf0kqn0dihgwmkzjhcyjc8xbp21js30ymszwrbmd5wbncr39n5";
+  #   # inherit (sources.emacs-overlay) owner repo rev sha256;
+  # }) self super);
 in {
 
   emacsGccWrapped = super.symlinkJoin {

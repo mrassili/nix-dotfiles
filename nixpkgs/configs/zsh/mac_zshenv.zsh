ccdff29f20214a15b6bf2914ae6adbfa9ff1572d
Michael Lingelbach <m.j.lbach@gmail.com>
2020-09-25 15:26:43 -0700

    Update

--- nixpkgs/configs/zsh/mac_zshenv.zsh
+++ nixpkgs/configs/zsh/mac_zshenv.zsh
@@ -18,8 +18,5 @@ path=($HOME/go/bin
 #   . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
 # fi
 
-eval `opam config env`
-
- test -r /Users/michael/.opam/opam-init/init.zsh && . /Users/michael/.opam/opam-init/init.zsh > /dev/null 2> /dev/null || true
 . "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"
 export NIX_PATH=$HOME/.nix-defexpr/channels${NIX_PATH:+:}$NIX_PATH

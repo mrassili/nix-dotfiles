c34a28cf944f73b8bdb27e953ef9173a8a871c7a
Michael Lingelbach <m.j.lbach@gmail.com>
2020-10-04 11:43:46 -0700

    Update

--- nixpkgs/configs/zsh/mac_zshenv.zsh
+++ nixpkgs/configs/zsh/mac_zshenv.zsh
@@ -20,3 +20,20 @@ path=($HOME/go/bin
 
 . "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"
 export NIX_PATH=$HOME/.nix-defexpr/channels${NIX_PATH:+:}$NIX_PATH
+
+conda() {
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/Users/michael/.local/miniconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/Users/michael/.local/miniconda3/etc/profile.d/conda.sh" ]; then
+        . "/Users/michael/.local/miniconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="/Users/michael/.local/miniconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+conda "$@"
+}

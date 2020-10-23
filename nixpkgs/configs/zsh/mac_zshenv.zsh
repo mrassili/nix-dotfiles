189b997282c7202c93115d26098687b9fbd34402
Michael Lingelbach <m.j.lbach@gmail.com>
2020-10-22 18:00:35 -0700

    Update

--- nixpkgs/configs/zsh/mac_zshenv.zsh
+++ nixpkgs/configs/zsh/mac_zshenv.zsh
@@ -13,25 +13,20 @@ path=($HOME/go/bin
       /usr/bin
       $path)
 
-#nix
-# if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
-#   . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
-# fi
-
-. "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"
+# Set nix path
 export NIX_PATH=$HOME/.nix-defexpr/channels${NIX_PATH:+:}$NIX_PATH
 
 conda() {
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$('/Users/michael/.local/miniconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+__conda_setup="$('$HOME/.local/miniconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
 if [ $? -eq 0 ]; then
     eval "$__conda_setup"
 else
-    if [ -f "/Users/michael/.local/miniconda3/etc/profile.d/conda.sh" ]; then
-        . "/Users/michael/.local/miniconda3/etc/profile.d/conda.sh"
+    if [ -f "$HOME/.local/miniconda3/etc/profile.d/conda.sh" ]; then
+        . "$HOME/.local/miniconda3/etc/profile.d/conda.sh"
     else
-        export PATH="/Users/michael/.local/miniconda3/bin:$PATH"
+        export PATH="$HOME/.local/miniconda3/bin:$PATH"
     fi
 fi
 unset __conda_setup

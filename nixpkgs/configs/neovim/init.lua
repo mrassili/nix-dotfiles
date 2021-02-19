bfa61c9d856ae3592e64b25cfb6fc347840b513e
Michael Lingelbach <m.j.lbach@gmail.com>
2021-02-19 11:57:55 -0800

    Add toggle for focusable window to get back out

--- nixpkgs/configs/neovim/init.lua
+++ nixpkgs/configs/neovim/init.lua
@@ -418,6 +418,28 @@ local on_attach = function(_client, bufnr)
     }
   )
 
+  -- Override to add 
+  vim.lsp.handlers['textDocument/hover'] = function(_, method, result)
+    vim.lsp.util.focusable_float(method, function()
+      if not (result and result.contents) then
+        -- return { 'No information available' }
+        return
+      end
+      local markdown_lines = vim.lsp.util.convert_input_to_markdown_lines(result.contents)
+      markdown_lines = vim.lsp.util.trim_empty_lines(markdown_lines)
+      if vim.tbl_isempty(markdown_lines) then
+        -- return { 'No information available' }
+        return
+      end
+      local bufnr, winnr = vim.lsp.util.fancy_floating_markdown(markdown_lines, {
+        pad_left = 1; pad_right = 1;
+      })
+      vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<Cmd>wincmd p<CR>', {noremap = true, silent = true})
+      vim.lsp.util.close_preview_autocmd({"CursorMoved", "BufHidden", "InsertCharPre"}, winnr)
+      return bufnr, winnr
+    end)
+  end
+
   -- Mappings.
   local opts = { noremap=true, silent=true }
   vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)

12a675a49cbb7f915d42eaf7213cd5a6edc2e925
Michael Lingelbach <m.j.lbach@gmail.com>
2021-06-01 13:30:22 -0700

    Update

--- nixpkgs/configs/neovim/init.lua
+++ nixpkgs/configs/neovim/init.lua
@@ -123,7 +123,7 @@ vim.g.indent_blankline_filetype_exclude = { 'help', 'packer' }
 vim.g.indent_blankline_buftype_exclude = { 'terminal', 'nofile'}
 vim.g.indent_blankline_char_highlight = 'LineNr'
 vim.g.indent_blankline_show_trailing_blankline_indent = false
-vim.g.indent_blankline_show_current_context = true
+-- vim.g.indent_blankline_show_current_context = false
 
 -- Toggle to disable mouse mode and indentlines for easier paste
 ToggleMouse = function()
